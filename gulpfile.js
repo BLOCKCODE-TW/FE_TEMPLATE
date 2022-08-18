7d8294eea7eabaa4e91b65a720c042095b3999a5
Justin Liao <justin.liao@outlook.com>
2022-08-18 14:13:12 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({lazy: false});
+const sass = require('gulp-sass')(require('sass'));
+const fileinclude = require('gulp-file-include');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-image');
+const browserSync = require('browser-sync').create();
+
+const project_name = 'DrSignal';
+
+const staticFiles = [
+    "./src/**/*",
+];
+
+const project_src = './src',
+      distPath = `./dist/${project_name}`;
+
+const source  = {
+
+  html: [
+    project_src + '/**/*.html',
+  ],
+
+  htmlInclude:[
+    project_src + '/partials/',
+  ],
+
+  js: [
+    project_src + '/**/*.js'
+  ],
+
+  scss: [
+    project_src + '/**/*.scss',
+    project_src + '/**/*.min.css',
+  ],
+
+  image: [
+    project_src + '/images/**/*.*',
+  ],
+
+}
+
+
+
+function clean() {
+    return gulp.src(distPath, {
+        read: false,
+        allowEmpty: true,
+    })
+        .pipe($.clean());
+}
+
+// 將sass編譯成css
+function buildStyles() {
+ return gulp.src(source.scss)
+    .pipe(sass())
+    .pipe(rename(function (path) {
+        path.dirname = path.dirname.replace('scss', 'css');
+    }))
+    .pipe(gulp.dest(distPath))
+    .pipe(
+      browserSync.reload({
+          stream: true,
+      }),
+    );
+}
+
+
+// HTML function
+function buildHtml() {
+  return gulp.src(source.html)
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest(distPath))
+    .pipe(
+      browserSync.reload({
+          stream: true,
+      }),
+    );
+}
+
+function buildScripts() {
+  return gulp.src(source.js)
+    .pipe(gulp.dest(distPath))
+    .pipe(
+        browserSync.reload({
+            stream: true,
+        }),
+    );
+}
+
+function compressImage(){
+  return gulp.src(source.image)
+    .pipe(imagemin({
+        progressive: true
+    }))
+    .pipe(gulp.dest(`${distPath}/images`))
+    .pipe(
+        browserSync.reload({
+            stream: true,
+        }),
+    );
+}
+
+// function copy() {
+//     return gulp.src(staticFiles)
+//         .pipe(gulp.dest(distPath))
+//         .pipe(
+//             browserSync.reload({
+//                 stream: true,
+//             }),
+//         );
+// }
+
+function synchronize() {
+    browserSync.init({
+        server: {
+            baseDir: distPath,
+        },
+        port: 8080,
+    });
+}
+
+function watch() {
+    gulp.watch(staticFiles, gulp.series(buildHtml, buildStyles, buildScripts, compressImage));
+}
+
+function deploy() {
+    return gulp.src(`${distPath}/**/*`)
+        .pipe($.ghPages());
+}
+
+exports.default = gulp.series(clean, buildStyles, buildHtml, buildScripts, compressImage, gulp.parallel(synchronize, watch));
+
+exports.clean = clean;
+exports.build = gulp.series(clean, buildHtml, buildStyles, buildScripts, compressImage);
+exports.deploy = deploy;
---
+++ readme.md
@@ -0,0 +1,35 @@
+# FE_TEMPLATE
+
+使用 Bootstrap 的前端開發範本
+
+## 環境需求
+
+node 14 以上
+
+## 安裝
+
+git clone
+
+yarn install
+
+## 啟動自動化作業
+
+在 vscode 環境下執行
+
+gulp
+
+## 開發注意事項
+
+修改 src 下的
+
+index.html
+
+## 其他參考
+
+Justin 提供的開發文件
+
+https://docs.google.com/document/d/19o2W8TGJwVfz86EbdtOArjxxu0WH7qFd4E8PBowTWdc/edit#
+
+橘子分享的 gulp 
+
+https://sharkcoder.com/tools/gulp
